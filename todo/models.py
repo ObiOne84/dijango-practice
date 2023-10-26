a19cf2243d7a86b7731b78c03e3f030c170c89b1
ObiOne84 <stepienszymon@hotmail.com>
2023-10-26 20:35:26 +0000

    Add model class Item and register model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    # null and blank equal false, is to prevent creating item without name,
+    # null prevents from creating items without name programmatically,
+    # blank=False, make the field required on forms
+    done = models.BooleanField(null=False, blank=False, default=False)
+
+    def __str__(self):
+        """
+        To overwrite default Django naming object, we add function that
+        returns the name of the added items. To add items you must go admin
+        panel, click on the items and click add
+        """
+        return self.name
